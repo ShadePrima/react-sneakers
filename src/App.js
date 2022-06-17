b7fd52a609e3c38a09a88268f84f41adfbfd7b81
ShadePrima <aleksandrag1984@gmail.com>
2022-06-17 16:46:51 +0300

    fetc

--- src/App.js
+++ src/App.js
@@ -6,71 +6,14 @@ import Header from './components/Header';
 
 
 function App() {
-  const [items, setItems] = React.useState([
-    {
-     "title": "Men’s Sneakers Nike Blazer Mid Suede",
-     "price": 100,
-     "imageUrl": "/img/sneakers/1.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Puma X Aka Boku Future Rider",
-     "price": 110,
-     "imageUrl": "/img/sneakers/2.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Under Armour Curry 8",
-     "price": 120,
-     "imageUrl": "/img/sneakers/3.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Nike Kyrie 7",
-     "price": 130,
-     "imageUrl": "/img/sneakers/4.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Puma X Aka Boku Future Rider",
-     "price": 126,
-     "imageUrl": "/img/sneakers/5.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Under Armour Curry 8",
-     "price": 96,
-     "imageUrl": "/img/sneakers/6.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Nike Kyrie 7",
-     "price": 108,
-     "imageUrl": "/img/sneakers/7.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Jordan Air Jordan 11",
-     "price": 104,
-     "imageUrl": "/img/sneakers/8.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Nike LeBron XVIII",
-     "price": 114,
-     "imageUrl": "/img/sneakers/9.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Nike Lebron XVIII Low",
-     "price": 119,
-     "imageUrl": "/img/sneakers/10.jpg"
-    },
-    {
-     "title": "Men’s Sneakers Nike Blazer Mid Suede",
-     "price": 142,
-     "imageUrl": "/img/sneakers/11.jpg"
-    },
-    {
-     "title": "Кроссовки Puma X Aka Boku Future Rider",
-     "price": 132,
-     "imageUrl": "/img/sneakers/12.jpg"
-    }
-   ])
+  const [items, setItems] = React.useState([])
   const [cartOpened, setCartOpened] = React.useState(false)
 
-
+fetch('https://62aafe60371180affbde9fc2.mockapi.io/items').then(res=> {
+  return res.json();
+}).then(json => {
+  setItems(json)
+});
 
 
   return (
